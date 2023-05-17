b4dae898698ad92e25a6afb0432026377409c4ed
Anton Krupin <krupin_anton@mail.ru>
2023-05-17 20:55:45 +0300

    add checkWhatsapp route

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@
 const apiPath = 'https://api.green-api.com/waInstance';
 const sendMessage = 'SendMessage';
 const getStateInstance = 'getStateInstance';
+const checkWhatsapp = 'CheckWhatsapp';
 const receiveNotification = 'receiveNotification';
 const deleteNotification = 'deleteNotification';
 
 export default {
 	authorization: (idInstance, apiTokenInstance) => [apiPath + idInstance, getStateInstance, apiTokenInstance].join('/'),
 	sendMessage: (idInstance, apiTokenInstance) => [apiPath + idInstance, sendMessage, apiTokenInstance].join('/'),
+	checkWhatsapp: (idInstance, apiTokenInstance) => [apiPath + idInstance, checkWhatsapp, apiTokenInstance].join('/'),
 	receiveNotification: (idInstance, apiTokenInstance) => [apiPath + idInstance, receiveNotification, apiTokenInstance].join('/'),
 	deleteNotification: (idInstance, apiTokenInstance, receiptId) => [apiPath + idInstance, deleteNotification, apiTokenInstance, receiptId].join('/'),
 }
