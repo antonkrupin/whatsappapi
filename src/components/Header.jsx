eb1edd1295e6a3b5309b3ad0603dc6a4a43593e5
antonkrupin <krupin_anton@mail.ru>
2023-05-17 13:04:00 +0300

    create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div className="d-flex justify-content-start">
+			<header>Green Api</header>
+		</div>
+	)
+};
+
+export default Header;
