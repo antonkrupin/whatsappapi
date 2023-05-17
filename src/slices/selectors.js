3ef4c0423a151d4aef09f9c41a30bbad4e93b858
antonkrupin <krupin_anton@mail.ru>
2023-05-17 13:03:29 +0300

    add selectors

--- src/slices/selectors.js
+++ src/slices/selectors.js
@@ -1,1 +1,4 @@
-export const fetchMessages = (state) => state.messages.messages;
+export const fetchMessages = (state) => state.chat.messages;
+export const fetchChartStart = (state) => state.chat.isChatStarted;
+export const fetchIdInstance = (state) => state.chat.idInstance;
+export const fetchApiTokenInstance = (state) => state.chat.apiTokenInstance;
