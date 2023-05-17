6fbfc0a566e6ffbdd8b0e99318ed0d02770fbf6e
Anton Krupin <krupin_anton@mail.ru>
2023-05-17 20:56:46 +0300

    create CreateChat component

--- src/components/CreateChat.jsx
+++ src/components/CreateChat.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useRef } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchIdInstance, fetchApiTokenInstance } from '../slices/selectors';
 import { setChatStart, setPhone } from '../slices/chatReducer';
+import routes from '../routes';
 
 const CreateChat = () => {
   useEffect(() => {
@@ -10,16 +12,50 @@ const CreateChat = () => {
   const dispatch = useDispatch();
 
   const inputRef = useRef();
+
+  const idInstance = useSelector(fetchIdInstance);
+
+	const apiTokenInstance = useSelector(fetchApiTokenInstance);
+
+  const [error, setError] = useState(null);
+
+  const startChat = (e) => {
+    e.preventDefault();
+    const phone = inputRef.current.value;
+    if (!isNaN(phone)) {
+      fetch(routes.checkWhatsapp(idInstance, apiTokenInstance), 
+      {
+        method: 'POST',
+        body: JSON.stringify(
+					{
+						"phoneNumber": phone
+					}
+				)
+      })
+      .then(response => response.text())
+		  .then(result => JSON.parse(result))
+      .then(data => {
+        if (data.existsWhatsapp) {
+          dispatch(setPhone(Number(phone)));
+          dispatch(setChatStart());
+        } else {
+          setError('На устройстве не установлено приложение WhatsApp. Проверьте ввёденный номер.');
+        }
+      });
+    } else {
+      setError('Номер телефона должен быть в формате 79001234567.');
+    }
+  }
+
   return (
     <div className="d-flex flex-column align-items-center">
-      <form onSubmit={() => dispatch(setChatStart())}>
+      <form onSubmit={startChat}>
         <div className="d-flex justify-content-center">
           <h3>Введите номер телефона, чтобы начать общение.</h3>
         </div>
         <div className="d-flex">
           <input
             required
-            onChange={(e) => dispatch(setPhone(e.target.value))}
             type="text"
             className="form-control"
             id="chatPhone"
@@ -35,6 +71,11 @@ const CreateChat = () => {
           </button>
         </div>
       </form>
+      {error && (
+        <div className="d-flex error">
+          {error}
+        </div>
+      )}
   </div>
   )
 };
